e10abc401f076aa1aa8c19db315f61e6f1c8eee8
金晓磊 <xiaolei.jin@fengyunzhibo.com>
2016-03-03 11:49:36 +0800

    creat project

---
+++ BurningBarrageDemo/BurningBarrageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BurningBarrageDemo
+//
+//  Created by i mac on 16/3/3.
+//  Copyright © 2016年 金小白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
